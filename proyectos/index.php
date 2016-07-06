4afb091e0985e7239071544897211b3d68d7de37
nicolasimh <nicolasimh@gmail.com>
2016-07-06 01:27:36 -0300

    Avances Vistas

--- proyectos/index.php
+++ proyectos/index.php
@@ -18,17 +18,55 @@
     </nav>
 
     <div class="clearfix" id="wrapper">
-		<div class="col-md-9" id="body-wrapper">
-
-			<p>asdf</p>
-			<p>asdf</p>
-		</div>
-		<div class="col-md-3">
+    	<div class="col-md-3 pull-right" id="option-wrapper">
 			<ul class="nav nav-pills nav-stacked">
  				<li role="presentation" class="active"><a href="#">Listado de Proyectos</a></li>
  				<li role="presentation"><a href="#">Profile</a></li>
  				<li role="presentation"><a href="#">Messages</a></li> 
 			</ul>
+		</div>
+		<div class="col-md-9 pull-left" id="body-wrapper">
+			<table class="table table-hover">
+				<thead>
+					<th>Cod</th>
+					<th>Nombre</th>
+					<th>Cliente</th>
+					<th>Estado</th>
+					<th></th>
+				</thead>	
+				<tbody>
+					<tr>
+						<td>1</td>
+						<td>Proyecto 1</td>
+						<td>Solsur</td>
+						<td><label>Terminado</label></td>
+						<td>
+							<button class="btn btn-sm">editar</button>
+							<button class="btn btn-sm">editar</button>
+						</td>
+					</tr>
+					<tr>
+						<td>2</td>
+						<td>Proyecto 2</td>
+						<td>Mas energía S.A.</td>
+						<td><label>En Curso</label></td>
+						<td>
+							<button class="btn btn-sm">editar</button>
+							<button class="btn btn-sm">editar</button>
+						</td>
+					</tr>
+					<tr>
+						<td>3</td>
+						<td>Proyecto 3</td>
+						<td>Clean Energy S.A.</td>
+						<td><label>Eliminado</label></td>
+						<td>
+							<button class="btn btn-sm">editar</button>
+							<button class="btn btn-sm">editar</button>
+						</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	</div>
 
