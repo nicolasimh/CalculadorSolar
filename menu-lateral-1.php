55a3b9bf334aff25b11ef7133db171be61fc0e9e
TallerIngenieria <Nicole>
2016-10-02 12:47:27 -0300

    manu lateral con color

--- menu-lateral-1.php
+++ menu-lateral-1.php
@@ -1,15 +1,17 @@
 <ul class="nav nav-pills nav-stacked">
-	<li role="presentation"><a href="../menu-configuracion.php">Configuración</a></li >
-	<li role="presentation" class="tituloMenuLateral">Mantener Usuarios</li>
-	<li role="presentation"><a href="../usuarios/nuevoUsuario.php">Nuevo Usuario</a></li>
-	<li role="presentation"><a href="../usuarios/index.php">Listado de Usuarios</a></li>
-	<li role="presentation" class="tituloMenuLateral">Mantener Clientes</li>			
-	<li role="presentation"><a href="../clientes/nuevoCliente.php">Nuevo Cliente</a></li>
-	<li role="presentation"><a href="../clientes/index.php">Listado de  Clientes</a></li>
-	<li role="presentation" class="tituloMenuLateral">Mantener Productos</li>			
-	<li role="presentation"><a href="../productos/nuevoProducto.php">Nuevo Producto</a></li>
-	<li role="presentation"><a href="../productos/index.php">Listado de  Productos</a></li>
-	<li role="presentation" class="tituloMenuLateral">Mantener Artefactos</li>			
-	<li role="presentation"><a href="../artefactos/nuevoArtefacto.php">Nuevo Artefacto</a></li>
-	<li role="presentation"><a href="../artefactos/index.php">Listado de  Artefactos</a></li>
+	<li role="presentation" class="btn btn-danger" class="tituloMenuLateral">Mantener Usuarios</li>
+	<li role="presentation" class="btn btn-danger" class="subtituloMenuLateral"><a href="../usuarios/nuevoUsuario.php">Nuevo Usuario</a></li>
+	<li role="presentation" class="btn btn-danger" class="subtituloMenuLateral"><a href="../usuarios/index.php">Listado de Usuarios</a></li>
+	<br>
+	<li role="presentation" class="btn btn-info" class="tituloMenuLateral">Mantener Clientes</li>			
+	<li role="presentation" class="btn btn-info"><a href="../clientes/nuevoCliente.php">Nuevo Cliente</a></li>
+	<li role="presentation" class="btn btn-info"><a href="../clientes/index.php">Listado de  Clientes</a></li>
+	<br>
+	<li role="presentation" class="btn btn-success" class="tituloMenuLateral">Mantener Productos</li>			
+	<li role="presentation" class="btn btn-success"><a href="../productos/nuevoProducto.php">Nuevo Producto</a></li>
+	<li role="presentation" class="btn btn-success"><a href="../productos/index.php">Listado de  Productos</a></li>
+	<br>
+	<li role="presentation" class="btn btn-warning" class="tituloMenuLateral">Mantener Artefactos</li>			
+	<li role="presentation" class="btn btn-warning"><a href="../artefactos/nuevoArtefacto.php">Nuevo Artefacto</a></li>
+	<li role="presentation" class="btn btn-warning"><a href="../artefactos/index.php">Listado de  Artefactos</a></li>
 </ul>
